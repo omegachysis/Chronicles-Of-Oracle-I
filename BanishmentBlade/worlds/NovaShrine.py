2c7e8bcf5e3c47ca35f113fa1bdef3e795473f39
Matthew Robinson <omegachysis@outlook.com>
2013-07-20 11:11:13 -0400

    CPython files produced

--- BanishmentBlade/worlds/NovaShrine.py
+++ BanishmentBlade/worlds/NovaShrine.py
@@ -5,10 +5,8 @@ def start(data):
     while True:
         if data["area"] == 0:
             narrateThought("""I thought my time was up.
-I thought I was done...
-""")
+I thought I was done...""")
 
             narrateVisual("""You wake up in a solitary dark chamber.
-The walls are wet, dripping and covered with overgrown plants.
-""")
+The walls are wet, dripping and covered with overgrown plants.""")
 
